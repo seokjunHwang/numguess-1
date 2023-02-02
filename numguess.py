6ee45c38bbd53a4f06a8399d717b1998185b1c3c
seokjunHwang <ada0004@naver.com>
2023-02-02 12:44:19 +0900

    fix : Change Type of 'guess' to Integer

--- numguess.py
+++ numguess.py
@@ -8,7 +8,7 @@ print(answer)
 # Get user's name, guess
 
 user_name = input('Hello, there! What is your name? ')
-guess = input(f'Hi, {user_name}. Guess the number(1-100): ')
+guess = int(input(f'Hi, {user_name}. Guess the number(1-100): '))
 
 # print to check
 print(user_name, guess)
